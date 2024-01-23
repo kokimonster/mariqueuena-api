85944935505265a38aa7fad2ffc2fd35031031d5
rnb <iralphbaluyot@gmail.com>
2024-01-24 03:37:17 +0800

    Login Registration, with package

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 
 // allows us to control the app's cross origin resource sharing
 const cors = require('cors');
-
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.use(express.json());
@@ -22,39 +22,47 @@ const db = mysql.createConnection({
     database: 'mariqueuena'
 })
 
-// app.get ('/users', (req,res)=>{
-//     const sql= "SELECT * FROM users";
-//     db.query(sql, (err,data)=> {
-//         if(err) return res.json(err);
-//         return res.json(data);
-//     })
-// })
+app.post('/login', async (req, res) => {
+    const sql = "SELECT * FROM users WHERE `email` = ?";
+    const values = [req.body.email];
 
-app.post('/users', (req, res) =>{
-    const sql = "SELECT * FROM users WHERE email = ? AND password = ?";
-    const values = [
-        req.body.email,
-        req.body.password
-    ]
-    db.query(sql, [values], (err, data) =>{
-        if(err) return res.json("Login Failed");
-        return res.json(data);
-    })
-})
+    db.query(sql, values, async (err, data) => {
+      if (err) {
+        return res.json("Login Failed");
+      }
+      
+      console.log("Data retrieved3:", data);
+      if (data.length > 0) {
+        const storedHashedPassword = data[0].password.trim();
 
-app.post('/registrationPage', (req, res) =>{
-    const sql = "INSERT INTO users (`fname`, `lname`, `email`, `password`) VALUES (?)";
-    const values = [
-        req.body.fName,
-        req.body.lName,
-        req.body.email,
-        req.body.password,
-    ]
-    db.query(sql, [values], (err, data) =>{
-        if(err) return res.json(err);
+        const passwordMatch = await bcrypt.compare(req.body.password.trim(), storedHashedPassword.trim());
+
+        if (passwordMatch) {
+          return res.json("Success");
+        } else {
+          return res.json("Failed");
+        }
+      } else {
+        return res.json("Failed");
+      }
+    });
+  });
+
+app.post('/registrationPage', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      const sql = "INSERT INTO users (`fname`, `lname`, `email`, `password`) VALUES (?, ?, ?, ?)";
+      const values = [req.body.fName, req.body.lName, req.body.email, hashedPassword];
+  
+      db.query(sql, values, (err, data) => {
+        if (err) return res.json(err);
         return res.json(data);
-    })
-})
+      });
+    } catch (error) {
+      console.error(error);
+      return res.json(error);
+    }
+  });
 
 // app.post('/register', (req, res)=> {
 //     const firstName = req.body.firstName;
